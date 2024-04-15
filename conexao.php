ffcb978cc6567b17d769ae5307b658d90af6bd0f
PauloHPS1991 <30605988+PauloHPS1991@users.noreply.github.com>
2024-04-15 10:05:40 -0300

    Update conexao.php

--- conexao.php
+++ conexao.php
@@ -1,8 +1,8 @@
 <?php
 
 	$dsn="mysql:host=localhost;dbname=id10581753_bancodados;charset=utf8";
-	$usuario = "id10581753_root";
-	$senha = "XXXXXXX";
+	$usuario = "";
+	$senha = "";
 
 	try{
 			$PDO = new PDO($dsn, $usuario, $senha);
